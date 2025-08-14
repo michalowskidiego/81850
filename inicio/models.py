fcbf5e5f641180394a782c7ada5afd101848d9d1
micha <michalowskidiego@gmail.com>
2025-08-13 21:04:16 -0300

    avance template

--- inicio/models.py
+++ inicio/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Deportes(models.Model):
+    deporte = models.CharField(max_length=20)
+    edad = models.CharField(max_length=20)
+    indumentaria = models.CharField(max_length=20)
